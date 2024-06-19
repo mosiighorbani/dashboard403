56db196fed1caf4b3b60dc96c2a0749808825030
mosiighorbani <mosi.php@gmail.com>
2024-06-20 02:14:59 +0330

    auth login is worked

--- auth/resources.py
+++ auth/resources.py
@@ -1,7 +1,6 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from flask_jwt_extended import create_access_token, create_refresh_token, get_jwt_identity, get_jwt, jwt_required
-from passlib.hash import pbkdf2_sha256
 from app import db
 from .models import UserModel
 from .schemas import UserSchema
@@ -10,6 +9,8 @@ from blocklist import BLOCKLIST
 
 
 authApi = Blueprint("Users", "users", description="Operations on users")
+
+
 
 @authApi.route("/api/auth/register")
 class UserRegister(MethodView):
@@ -21,13 +22,25 @@ class UserRegister(MethodView):
         user = UserModel()
         user.name = user_data["name"]
         user.phone = user_data["phone"]
-        user.password = pbkdf2_sha256.hash(user_data["password"])
+        user.set_password(user_data["password"])
         # 
         db.session.add(user)
         db.session.commit()
         # 
         return {"message" : "User is Created Successfully"}, 201
     
+
+
+@authApi.route("/api/auth/login")
+class UserLogin(MethodView):
+    @authApi.arguments(UserSchema)
+    def post(self, user_data):
+        user = UserModel.query.filter(UserModel.phone == user_data["phone"]).first()
+        if user and user.check_password(user_data["password"]):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {"access_token" : access_token, "refresh_token" : refresh_token}
+        abort(401, message="Invalid Credentilas")
 
 
 
